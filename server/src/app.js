0dedaae955428373c7afb9802c961fb6264eb7d8
Gem-Yashita-Khurana <yashita.khurana@geminisolutions.com>
2022-05-22 00:32:15 +0530

    connecting mongoDB with Nodejs

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,12 @@
 const express = require('express');
+//importing the database into the server
+const dbConnect = require('./config/dbConnect');
 
+//connecting the db
+dbConnect();
 const app = express();
 module.exports=app;
+
+//lprArypKGVp4DN0c
+
+//mongodb+srv://Yashita:<password>@cluster0.mn1pf.mongodb.net/?retryWrites=true&w=majority
