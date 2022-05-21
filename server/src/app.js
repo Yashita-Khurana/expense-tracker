478ccc13e3405f89fc09937829cf0892e35e38d6
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-05-22 02:41:13 +0530

    added comments

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
+// require express for setting up the express server
 const express = require('express');
+
 //importing the database into the server
 const dbConnect = require('./config/dbConnect');
 
 //connecting the db
 dbConnect();
+// using express
 const app = express();
 module.exports=app;
 
